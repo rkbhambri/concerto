beac7829571984514637a6ffea48fc8cbd023ed5
rkbhambri <ramkrishan@redbaron.in>
2020-06-28 16:17:03 +0530

    Updates

---
+++ precache-manifest.d690b79e3567f6f78a2afe44c93eb861.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "befb24b3f6f670975d37e1cc2853225a",
+    "url": "/concerto/index.html"
+  },
+  {
+    "revision": "e077701d1903d6340f8b",
+    "url": "/concerto/static/css/main.344c2313.chunk.css"
+  },
+  {
+    "revision": "600a01a13ff26e8a296c",
+    "url": "/concerto/static/js/2.127faf5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/concerto/static/js/2.127faf5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e077701d1903d6340f8b",
+    "url": "/concerto/static/js/main.35deebe4.chunk.js"
+  },
+  {
+    "revision": "a8685a845c96e9f44b5c",
+    "url": "/concerto/static/js/runtime-main.6cab7ea3.js"
+  },
+  {
+    "revision": "216dc7395dfb71b89d9c4df28b599cfc",
+    "url": "/concerto/static/media/home-banner.216dc739.png"
+  },
+  {
+    "revision": "1a97bf043d5a9b6d1c7eda7dbba32754",
+    "url": "/concerto/static/media/undraw_career_progress_ivdb.1a97bf04.png"
+  },
+  {
+    "revision": "5b587ca5cfc51e7b5729c34a1dc222e5",
+    "url": "/concerto/static/media/undraw_christmas_tree_56sw.5b587ca5.png"
+  },
+  {
+    "revision": "6118c0f03229ede2f46a9b12b111da44",
+    "url": "/concerto/static/media/undraw_developer_activity_bv83.6118c0f0.png"
+  },
+  {
+    "revision": "3ba5c935ece304b442e32ee6828e1eb4",
+    "url": "/concerto/static/media/undraw_fitness_tracker_3033.3ba5c935.png"
+  },
+  {
+    "revision": "3179b52b976d4147c309835187cb97b6",
+    "url": "/concerto/static/media/undraw_hiking_d24r.3179b52b.png"
+  }
+]);
